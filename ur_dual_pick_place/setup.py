e6c57dfdc6b4fe0d9d5f0e029ac7260829a99654
Danrod0 <130868063+Danrod0@users.noreply.github.com>
2026-06-15 17:06:30 +0200

    Update setup.py
    Limpiando enfoques viejos

--- ur_dual_pick_place/setup.py
+++ ur_dual_pick_place/setup.py
@@ -26,20 +26,10 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'pose_transformer = ur_dual_pick_place.pose_transformer_node:main',
-            'visual_pregrasp_moveitpy = ur_dual_pick_place.visual_pregrasp_moveitpy_node:main',
-            'move_to_home_right = ur_dual_pick_place.move_to_home_right_node:main',
-            'pregrasp_step1_hello = ur_dual_pick_place.pregrasp_step1_hello:main',
-            'pregrasp_step2_named_pose = ur_dual_pick_place.pregrasp_step2_named_pose:main',
-            'pregrasp_step3_fixed_pose = ur_dual_pick_place.pregrasp_step3_fixed_pose:main',
-            'pregrasp_step3b_measured_pose = ur_dual_pick_place.pregrasp_step3b_measured_pose:main',
-            'ur_dual_home_test = ur_dual_pick_place.ur_dual_home_test:main',
             'ur_dual_command_services = ur_dual_pick_place.ur_dual_command_services:main',
             'object_pose_bridge = ur_dual_pick_place.object_pose_bridge:main',
-            'depth_crop_to_cloud = ur_dual_pick_place.depth_crop_to_cloud:main',
             'obstacle_clusterer = ur_dual_pick_place.obstacle_clusterer:main',
             'octomap_input_filter = ur_dual_pick_place.octomap_input_filter:main',
-	    'visual_pregrasp = ur_dual_pick_place.visual_pregrasp_node:main',
         ],
     },
 )
